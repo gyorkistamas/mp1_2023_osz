dd5c209a216440cfc4e266db7167c411d2f0d859
gyorkistamas <tamasgyorkis@gmail.com>
2023-12-05 16:58:24 +0100

    zh befejezése

--- b_kurzus/19_zh_gyak_csoki/Program.cs
+++ b_kurzus/19_zh_gyak_csoki/Program.cs
@@ -22,6 +22,53 @@ namespace _19_zh_gyak_csoki
 
     internal class Program
     {
+
+        static int F6(Csoki csoki)
+        {
+            if (csoki.szavatossag < DateTime.Now)
+            {
+                return 0;
+            }
+
+            if (csoki.tejcsokolade)
+            {
+                csoki.ar = (int)Math.Round(csoki.ar * 0.75);
+            }
+            else
+            {
+                csoki.ar = (int)Math.Round(csoki.ar * 0.70);
+            }
+
+            if (csoki.mennyiseg >= 60)
+            {
+                csoki.ar = (int)Math.Round(csoki.ar * 0.94);
+            }
+
+            return csoki.ar;
+        }
+        static void F5(List<Csoki> lista, int ar, List<Csoki> valogatott)
+        {
+            valogatott.Clear();
+
+            for(int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].ar > ar)
+                    valogatott.Add(lista[i]);
+            }
+        }
+        
+        static bool F3_eldönt(List<Csoki> csokik, string gyarto, string izesites)
+        {
+            for (int i = 0; i < csokik.Count; i++)
+            {
+                if (csokik[i].marka == gyarto && csokik[i].izesites == izesites)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         static void Main(string[] args)
         {
             // 1. feladat
@@ -77,7 +124,95 @@ namespace _19_zh_gyak_csoki
             }
 
             Console.WriteLine($"A kidobott mennyiség: {Math.Round(kidobottTomeg / 1000, 2)} kg");
+
+            Console.Write("Adja meg a gyártót:");
+            string gyarto = Console.ReadLine();
+            Console.Write( "Adja meg az ízesítést:");
+            string izesites= Console.ReadLine();
+
+            if (F3_eldönt(csokik, gyarto, izesites))
+            {
+                for (int i = 0; i < csokik.Count; i++)
+                {
+                    if (csokik[i].marka == gyarto && csokik[i].izesites == izesites)
+                    {
+                        Console.WriteLine($"{csokik[i].azonosito}, {csokik[i].tomeg}, {csokik[i].ar}, {(csokik[i].mennyiseg <= 0 ? "nincs raktáron " : "van raktáron")} ");
+                    }
+
+                }
+            }
+            else Console.Write("Nincs ilyen csoki a raktáron.");
+
+            // 7. feladat
+            List<Csoki> szűrtLista = new List<Csoki>();
+            F5(csokik, 650, szűrtLista);
+
+            Console.WriteLine("7. feladat");
+
+            foreach(Csoki csoki in szűrtLista)
+            {
+                if (csoki.szavatossag >= DateTime.Now)
+                {
+                    Console.WriteLine($"{csoki.azonosito}: {csoki.marka} ({csoki.izesites}) Ár: {csoki.ar} Ft, Akciós ár: {F6(csoki)} Ft");
+                }
+            }
+            //8.feladat
+            int torlendoIndex = -1;
+            for (int i = 0;i < csokik.Count; i++)
+            {
+                if (csokik[i].azonosito == "NBKL5NQ")
+                {
+                    torlendoIndex = i;
+                    break;
+                }
+            }
+            if (torlendoIndex == -1) Console.WriteLine("Nincsen benne az adott termék.");
+            else 
+            {
+                csokik.RemoveAt(torlendoIndex);
+                Console.WriteLine("A törlés sikeres volt!");
+            }
+            
+            //9. feladat
+            List<string> gyartok = new List<string>();
+            for (int i = 0; i < csokik.Count; i++)
+            {
+                bool benneVanE = false;
+                for (int j = 0; j < gyartok.Count; j++)
+                {
+                    if (gyartok[i] == csokik[i].marka)
+                    {
+                        benneVanE = true;
+                    }
+                }
+                if (!benneVanE) 
+                {
+                    gyartok.Add(csokik[i].marka);
+                }
+
+                //if (!gyarto.Contains(csokik[i].marka))
+                //    gyartok.Add(csokik[i].marka);
+            }
+
+            for (int i = 0; i < gyartok.Count; i++)
+            {
+                Csoki max = new Csoki();
+                max.ar = -1;
+                for (int j = 0;j < csokik.Count; j++)
+                {
+                    if (csokik[j].marka == gyartok[i] && csokik[j].ar > max.ar)
+                    {
+                        max = csokik[j];
+                    }
+                }
+                Console.WriteLine($"A {gyartok[i]} gyártónak a legdrágább csokiának az ízesítése: {max.izesites}");
+                Console.WriteLine($"Ha minden csokit eladnánk akkor {max.mennyiseg * max.ar} Ft lenne a profitunk");
+            }
+
             Console.ReadLine();
+
+
+
         }
     }
 }
